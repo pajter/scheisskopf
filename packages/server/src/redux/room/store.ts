70820ac2d005b63d1521e2e5e7777fc6c0176c2a
Pieter Janssen <4687577+pajter@users.noreply.github.com>
2020-04-15 20:54:21 +0200

    Add redux devtools

--- packages/server/src/redux/room/store.ts
+++ packages/server/src/redux/room/store.ts
@@ -1,9 +1,20 @@
 import { createStore } from 'redux';
-
+import devToolsEnhancer from 'remote-redux-devtools';
 import { reducer, initialState as _initialState } from './reducer';
 import { Store, State } from './types';
 
 export let store: Store;
+
+let devTools =
+  process.env.NODE_ENV === 'development'
+    ? devToolsEnhancer({
+        realtime: true,
+        shouldCatchErrors: false,
+        shouldHotReload: true,
+        hostname: 'localhost',
+        port: 8000,
+      })
+    : undefined;
 
 export const getStore = (initialState?: Partial<State>) => {
   if (store) {
@@ -11,7 +22,7 @@ export const getStore = (initialState?: Partial<State>) => {
   }
 
   // Create store
-  store = createStore(reducer, { ..._initialState, ...initialState });
+  store = createStore(reducer, { ..._initialState, ...initialState }, devTools);
 
   return store;
 };
